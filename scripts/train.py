caa0ab499b59809e0da0994861ae670218a45baf
chirilcalin <167574104+chirilcalin@users.noreply.github.com>
2025-03-08 12:37:51 -0500

    Update train.py

--- scripts/train.py
+++ scripts/train.py
@@ -6,10 +6,15 @@ import torch
 import torch.nn as nn
 import numpy as np
 import math
-from easy_transformer.utils import get_corner, gelu_new, tokenize_and_concatenate
+from easy_transformer.utils import tokenize_and_concatenate
 import tqdm.auto as tqdm
 import time
+import datasets
+import transformers
+import plotly.express as px
 
+
+from ropetransformer import RopeTransformer
 
 reference_gpt2 = EasyTransformer.from_pretrained("gpt2-small", fold_ln=False, center_unembed=False, center_writing_weights=False)
 
@@ -37,13 +42,13 @@ weight_decay = 1e-2
 model_cfg = Config(debug=False, d_model=256, n_heads=4, d_head=64, d_mlp=1024, n_layers=2, n_ctx=256, d_vocab=reference_gpt2.cfg.d_vocab)
 
 
-demo_gpt2 = RopeTransformer(Config(debug=False))
-demo_gpt2.load_state_dict(reference_gpt2.state_dict(), strict=False)
-demo_gpt2.cuda()
-#test_string = """Mini scule is a species of microhylid frog endemic to Madagascar that was described in 2019. The scientific name of the species refers to its size, being a pun on the word minuscule. It is very small, measuring only 8.4 to 10.8 mm (0.33 to 0.43 in) in snout–vent length. It has bronze underparts with a brown groin and back of the thigh, cream upperparts with brown flecking, a dark brown side of the head, and a red iris. On the hind feet, the first toe is absent and the second and fifth toes are strongly reduced. The frog is known only from the Sainte Luce Reserve, where it inhabits areas with deep leaf litter near semi-permanent water bodies. Specimens of frogs from Mandena, the Vohimena mountains, the southern Anosy Mountains, and Tsitongambarika may also be of this species. Along with Mini mum and Mini ature, the other two species in its genus, it received media attention when first described due to the wordplay in its scientific name. (Full article...)"""
+dataset = datasets.load_dataset("NeelNanda/pile-10k", split="train")
+tokens_dataset = tokenize_and_concatenate(dataset, reference_gpt2.tokenizer, streaming=False, max_length=model_cfg.n_ctx, column_name="text", add_bos_token=True, num_proc=4)
+data_loader = torch.utils.data.DataLoader(tokens_dataset, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True)
 
-test_tokens = reference_gpt2.to_tokens(test_string).cuda()
-demo_logits = demo_gpt2(test_tokens)
+model = RopeTransformer(model_cfg)
+model.cuda()
+
 
 
 def lm_cross_entropy_loss(logits, tokens):
@@ -53,32 +58,10 @@ def lm_cross_entropy_loss(logits, tokens):
     log_probs = logits.log_softmax(dim=-1)
     pred_log_probs = log_probs[:, :-1].gather(dim=-1, index=tokens[:, 1:].unsqueeze(-1)).squeeze(-1)
     return -pred_log_probs.mean()
-loss = lm_cross_entropy_loss(demo_logits, test_tokens)
-print(loss)
-print("Loss as average prob", (-loss).exp())
-print("Loss as 'uniform over this many variables'", (loss).exp())
-print("Uniform loss over the vocab", math.log(demo_gpt2.cfg.d_vocab))
 
 
-#test_string = "Breaking News: President Trump has been impeached by the House of Representatives for abuse of power and obstruction of Congress. The vote was 230 to 197, with 10 Republicans joining all Democrats in voting to impeach. The president is now only the third in American history to be impeached, and the first to be impeached twice. The House will now send the articles of impeachment to the Senate, where a trial will be held to determine whether to remove the president from office. The Senate is expected to begin the trial on"
-for i in tqdm.tqdm(range(100)):
-    test_tokens = reference_gpt2.to_tokens(test_string).cuda()
-    demo_logits = demo_gpt2(test_tokens)
-    test_string += reference_gpt2.tokenizer.decode(demo_logits[-1, -1].argmax())
-print(test_string)
 
-import datasets
-import transformers
-import plotly.express as px
 
-dataset = datasets.load_dataset("NeelNanda/pile-10k", split="train")
-print(dataset)
-print(dataset[0]['text'][:100])
-tokens_dataset = tokenize_and_concatenate(dataset, reference_gpt2.tokenizer, streaming=False, max_length=model_cfg.n_ctx, column_name="text", add_bos_token=True, num_proc=4)
-data_loader = torch.utils.data.DataLoader(tokens_dataset, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True)
-
-model = RopeTransformer(model_cfg)
-model.cuda()
 
 optimizer = torch.optim.AdamW(model.parameters(), lr=lr, weight_decay=weight_decay)
 
